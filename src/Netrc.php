effca9b1071153fd10fe688d0f92ab8a7f6bfb94
Mark A. Hershberger <mah@nichework.com>
2019-05-31 16:24:50 -0400

    Add method to get the default path

--- src/Netrc.php
+++ src/Netrc.php
@@ -22,6 +22,27 @@ use Fduch\Netrc\Exception\ParseException;
 class Netrc
 {
     /**
+     * Get the default path of the netrc file that will be used if one
+     * is not provided.
+     *
+     * @return string
+     */
+    public static function getDefaultPath() {
+        $homePath = getenv('HOME');
+        if (!homePath) {
+            throw new ParseException(
+                "HOME environment variable must be set for correctly netrc handling"
+            );
+        }
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $filename = strtr($homePath, '\\', '/') . '/_netrc';
+        } else {
+            $filename = rtrim($homePath, '/') . '/.netrc';
+        }
+        return $filename;
+    }
+
+    /**
      * Parses netrc file specified or default one
      *
      * @param string|null $filename
@@ -34,14 +55,7 @@ class Netrc
     {
         // fetch netrc filename if it is not specified
         if (!$filename) {
-            if (!$homePath = getenv('HOME')) {
-                throw new ParseException("HOME environment variable must be set for correctly netrc handling");
-            }
-            if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-                $filename = strtr($homePath, '\\', '/') . '/_netrc';
-            } else {
-                $filename = rtrim($homePath, '/') . '/.netrc';
-            }
+            $filename = self::getDefaultPath();
         }
         $filename = realpath($filename);
 
